4e62ce4abe5b3a0a8454bafb75d5eae7905ce769
Nahuel <salvatierra.nahuel@gmail.com>
2025-07-06 02:24:09 -0300

    Update CurrentUser decorator to verify Firebase ID token and extract user information

--- src/modules/iam/authentication/infrastructure/decorator/current-user.decorator.ts
+++ src/modules/iam/authentication/infrastructure/decorator/current-user.decorator.ts
@@ -1,13 +1,17 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
-
-import { REQUEST_USER_KEY } from '@iam/iam.constants';
+import * as admin from 'firebase-admin';
 
 import { CurrentUserDto } from '../../domain/currentUser.dto';
 
 export const CurrentUser = createParamDecorator(
-  (field: keyof CurrentUserDto, ctx: ExecutionContext) => {
+  async (field: keyof CurrentUserDto, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    const user: CurrentUserDto = request[REQUEST_USER_KEY];
+    const token = request.headers.authorization.split(' ')[1];
+    const payload = await admin.auth().verifyIdToken(token);
+    const user: CurrentUserDto = {
+      externalId: payload.uid,
+      username: payload.email,
+    };
     return field ? user?.[field] : user;
   },
 );
