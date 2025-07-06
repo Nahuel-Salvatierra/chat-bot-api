d4f51434de90978d87bbb6e5b5c50364658bd350
Nahuel <salvatierra.nahuel@gmail.com>
2025-07-06 02:24:03 -0300

    Add environment variables for NODE_ENV, Firebase database URL, and OpenAI API key in configuration

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -1,4 +1,5 @@
 export const configuration = () => ({
+  nodeEnv: process.env.NODE_ENV,
   server: {
     port: Number(process.env.PORT),
   },
@@ -9,5 +10,9 @@ export const configuration = () => ({
     projectId: process.env.FIREBASE_PROJECT_ID,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+  },
+  openai: {
+    apiKey: process.env.OPEN_API_KEY,
   },
 });
