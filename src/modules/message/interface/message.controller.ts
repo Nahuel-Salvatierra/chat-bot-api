827678e88b73d2e849e1b4b5846f2072c0d5a21b
Nahuel <salvatierra.nahuel@gmail.com>
2025-07-06 02:25:06 -0300

    Refactor MessageController to support message retrieval and creation, updating endpoint to 'messages' and integrating message service for handling user messages.

--- src/modules/message/interface/message.controller.ts
+++ src/modules/message/interface/message.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
-@Controller('message')
+import { CurrentUserDto } from '@iam/authentication/domain/currentUser.dto';
+import { CurrentUser } from '@iam/authentication/infrastructure/decorator/current-user.decorator';
+
+import { CreateMessageDto } from '../application/dto/create-message-dto';
+import { MessageService } from '../application/service/message.service';
+
+@Controller('messages')
 export class MessageController {
+  constructor(private readonly messageService: MessageService) {}
+
   @Get()
-  getMessage() {
-    return {
-      message: 'Hello World',
-    };
+  async getMessages(@CurrentUser() user: CurrentUserDto) {
+    return await this.messageService.getMessages(user.externalId);
+  }
+
+  @Post()
+  async createMessage(
+    @CurrentUser() user: CurrentUserDto,
+    @Body() dto: CreateMessageDto,
+  ) {
+    return await this.messageService.createMessage(dto, user.externalId);
   }
 }
