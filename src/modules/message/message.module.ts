4b56fc415ef7135d66e224938715532377436d45
Nahuel <salvatierra.nahuel@gmail.com>
2025-07-06 02:24:31 -0300

    Enhance MessageModule by integrating OpenAiService and correcting FirebaseMessageRepository import path

--- src/modules/message/message.module.ts
+++ src/modules/message/message.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { MESSAGE_REPOSITORY_KEY } from './application/repository/message.repository.interface';
+import { IA_SERVICE_KEY } from './application/service/ai.service.interface';
 import { MessageService } from './application/service/message.service';
-import { FirebaseMessageRepository } from './infrastructure/persistance/firebase-message.repository';
+import { OpenAiService } from './application/service/openapi.service';
+import { FirebaseMessageRepository } from './infrastructure/persistence/firebase-message.repository';
 import { MessageController } from './interface/message.controller';
 
 @Module({
@@ -12,8 +14,11 @@ import { MessageController } from './interface/message.controller';
       provide: MESSAGE_REPOSITORY_KEY,
       useClass: FirebaseMessageRepository,
     },
+    {
+      provide: IA_SERVICE_KEY,
+      useClass: OpenAiService,
+    },
     MessageService,
   ],
-  exports: [],
 })
 export class MessageModule {}
